b6a78099eb185728631e6b241edc35b02373256f
helloavery <itszjersey@aim.com>
2017-01-24 19:52:17 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="See who Avery Grimes-Farrow is- An upcoming creative I.T. and Data professional
+          who is up for the challenges that the coding and developing world throws.">
+        <meta name="keywords" content="HTML,CSS,XML,JavaScript, Information Technology, I.T., Programming,
+            Java, PHP, SQL, Data Mining, Analytics, Databases, Professional, Creative, Web Design ">
+        <meta name="author" content="Avery Grimes-Farrow">
+        <title>Avery Grimes-Farrow</title>
+        
+        <link rel="shortcut icon" href="photos/favicon.png">
+        
+   
+
+<!-- Fonts/Google Fonts -->
+
+<script data-cfasync="false" type="text/javascript">try{Typekit.load();}catch(e){}</script>
+<style type="text/css">
+.wf-loading .blog-title,
+.wf-loading .post-title {
+/* Hide the blog title and post titles while web fonts are loading */
+visibility: hidden;
+}
+
+</style>  
+
+        
+<script type='text/javascript' src='js/jquery.js'></script>
+   
+                
+               
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Merriweather:400,900italic,300,700,900|Cinzel:400,900,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Montserrat|Cardo' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Megrim|Miriam+Libre|Raleway:700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Caveat:700|Covered+By+Your+Grace|Just+Another+Hand|Kalam:300|Satisfy" rel="stylesheet"> 
+<!-- End of Fonts -->
+                
+<link rel="stylesheet" type="text/css" href="css/stylesheetindex.css">
+<link href="css/lightbox.css" rel="stylesheet">
+
+<!-- Bootstrap -->
+
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+    
+    
+  <link href="css/carousel.css" rel="stylesheet">
+  <link href="css/newstyle.less" rel="stylesheet">
+  </head>
+
+    <body>
+        <?php include_once("analyticstracking.php") ?>
+
+        <!-- NAVBAR -->
+        
+        <header class="main_h" id="sticky">
+
+    <div class="row_nav">
+        <p class="logo" >A.GF</p>
+
+        
+        
+        <div class="mobile-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <nav>
+            <ul>
+                
+                <li><a href="index.php" style="color:#4372AA;">ABOUT</a></li>
+                <li><a href="experience.php">EXPERIENCE</a></li>
+                <li><a href="education.php">EDUCATION</a></li>
+                <li><a href="skills.php">SKILLS</a></li>
+                <li><a href="contact.php">CONTACT</a></li>
+            </ul>
+        </nav>
+
+    </div> <!-- / row -->
+
+</header>
+        
+
+
+     <!-- <div class="breadcrumb breadcrumb-fullscreen alignleft small-description overlay almost-black-overlay" 
+           style="width:100%;padding:0;">
+          
+<div class="starTitle">Hello, hello World.</div>-->
+<div class="topbox">
+    
+    Hello, hello World.
+    <div style="position: absolute; bottom: 0; right:0; width: auto; text-align:right;font-size:12px;">
+Art Credit:http://kotopez.deviantart.com/</div>
+</div>     
+
+          <!--<div class="col-sm-12">
+<canvas class="cover" style="width:100%;"></canvas>
+         <!-- </div>
+<div class="js-height-full star"></div>
+<div class="star-pattern-1 js-height-full" style="height: 994px;"></div>
+      </div>
+-->
+  
+
+
+
+
+      <!-- Three columns of text below the carousel -->
+
+
+      <div class="container marketing">
+          <h1> What's New?</h1>
+          <hr class="featurette-divider" style="margin:2%;">
+          
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-3">
+          <img class="img-circle" src="icons/User-96.png" alt="Generic placeholder image" height="96" width="96">
+          <h2>User Accounts</h2>
+          <p>Hold on tight, because user accounts are coming.</p>
+          <p>Accounts will be launched with blogs...Why yes, you will get to comment on artcles.</p>
+          <p></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-3">
+          <img class="img-circle" src="icons/Hashtag 2-96.png" alt="Generic placeholder image" height="96" width="96">
+          <h2>Blogs</h2>
+          <p>I'm at a place where I am satisfied with the framework and general layout of the blogs section.</p>
+          <p>Right now I am in back-end mode setting up the databases (blogs and user accounts) and linking them (via PHP).</p>
+          <p>I'm almost there. Closer than Katy Perry getting a Grammy close.</p>
+          <!-- Trigger/Open The Modal 
+          <p><a class="example-image-link" href="photos/imnotlying.jpg" data-lightbox="example-2" data-title="See I'm not lying.">
+    <button class="btn btn-default" role="button" >View details »></button></a> </p>-->
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-3">
+          <img class="img-circle" src="icons/source_code-96.png" alt="Generic placeholder image" height="96" width="96">
+          <h2>New Design</h2>
+          <p>Still working on that <strike>dang</strike> <i>lovely</i> nav bar.</p>
+          <p>Smaller screens will not have to suffer for much longer.</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-3">
+            <img class="img-circle" src="icons/likes_folder-96.png" alt="Generic placeholder image" height="96" width="96">
+          <h2>Media Refresh</h2>
+          <p>Still chopping through objects, iframes, and JavaScript to provide for a better experience.</p><p> It's still the perfect time to
+          use your favorite Lush bath bomb or face mask (Rosy Cheeks and Mask of Magnamity are my personal favorite) and
+          look at some of my work :)</p>
+        
+          <p></p>
+        </div><!-- /.col-lg-4 -->
+        
+        
+      </div><!-- /.row -->
+
+<!-- The Modal -->
+
+<!--
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  
+  <!--
+  <div class="modal-content">
+    <div class="modal-header">
+        <span class="close">&times;close</span>
+      <h2>See I'm really working on</h2>
+    </div>
+    <div class="modal-body">
+      <img src='photos/imnotlying.jpg' style="width:70%">
+    </div>
+  </div>
+
+</div>
+      
+      -->
+      
+   
+
+      
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider" style="margin:2%;">
+
+
+      <div class="row featurette">
+        <div class="col-md-7">
+            <h2 class="featurette-heading" style="margin-bottom:0;">Oh, hello.</h2>
+                <h2 class="featurette-heading" style="margin-top:0;"><span class="text-muted">Yes. It is me that you see.</span></h2>
+          <p class="lead">Currently a recent graduate from Hofstra University. 
+                Currently I am looking for opportunities starting Q1 2017. 
+                Open to anything in the Information Technology and/or Analytics 
+                realm to expand my knowledge and connections.</p>
+          <p>I'm a high achiever easily able to adopt to various environments. 
+               High people skills with experience in a heavy customer service industry. 
+               Analytical mind able to figure out complex problems. 
+               Works well individually and in groups; 
+               able to take charge in situations and lead teams through convoluted scenarios. 
+               Others trust me because of my ability to take on arduous ventures which require 
+               quick deliberating and patience. </p>
+        </div>
+        <div class="col-md-5">
+            <img data-holder-rendered="true" src="photos/picresized.jpg" class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="500x500">
+        </div>
+      </div>
+
+      <hr class="featurette-divider" style="margin:2%;">
+
+      <!-- /END THE FEATURETTES --> 
+
+
+
+      <!-- FOOTER -->
+     <div id="footer" style="margin-top:0.1%"><h5>itavery.com | Avery Grimes-Farrow</h5>
+    <table>
+        <tr>
+            <td><h6><div id="clockdiv"><span class="days"></span></div></h6></td>
+            <td><h6>0</h6></td>
+            <td><h6>&#8734;</h6></td>
+        </tr>
+        <tr>
+            <td>Days since graduation</td>
+            <td>Times I've gone to bed before 1AM this month</td>
+            <td>Amount of times I've asked myself <br>"What did I do to deserve this?"
+             <br>while I do one of my favorite things - Coding and Developing </td>
+        </tr>
+    </table>
+
+<br>
+<footer>
+          
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>· 2017 itavery.com | Avery Grimes-Farrow ·</p>
+      </footer>
+</div>
+
+    </div>
+
+ 
+<script src="js/navbar.js"></script>
+<script src="js/timer.js"></script>
+<script src="js/lightbox.js"></script>
+
+
+
+<!--Script for staroverlay-->
+
+<script type='text/javascript' src='js/aqura_main.js'></script>
+<script type='text/javascript' src='js/aqura_plugins.js'></script>
+
+
+
+
+<!--Script for popup text box-->
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+          
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs>
+<style type="text/css"></style></defs><text x="0" y="25" style="font-weight:bold;font-size:25pt;font-family:Arial, Helvetica, Open Sans, sans-serif">500x500</text></svg>
+            
+
+    </body>
+
+          
+</html>
+
